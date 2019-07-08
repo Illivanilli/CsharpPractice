51c91889aae26d9a098da7664a35c5e612d3c351
Christopher Parson <cparson23@gmail.com>
2019-07-08 10:47:42 -0500

    login as it's own class

---
+++ Practice/Practice/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using RedditSharp;
+using RedditSharp.Things;
+namespace Practice
+{
+    class Login
+    {
+        //login to reddit using the old way, then the new way QAuth
+        public Login()
+        {
+            var login = new Reddit();
+            var user = login.LogIn("username", "Password");  // create new Reddit, then create user to login to that reddit. LogIn(username, pass);
+
+
+
+            var mysubreddit = login.GetSubreddit("/r/Illisontestbot");
+
+        }
+
+
+    }
+}
